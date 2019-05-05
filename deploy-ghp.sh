c6d71f6b0ae671b76598a4a1ba9777b612a6ba94
priyank-sriv <priyank.srivastava@appdirect.com>
2019-05-05 21:24:14 +0530

    deploy script to add submodule push

--- deploy-ghp.sh
+++ deploy-ghp.sh
@@ -2,16 +2,22 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+
 # Build the project.
 hugo -t cupper-hugo-theme  # if using a theme, replace with `hugo -t <YOURTHEME>`
 
+
 # Update submodule
-git submodule update
+git submodule update --remote --merge
+
 
 # Go To Public folder
 cd public
+
+
 # Add changes to git.
 git add .
+
 
 # Commit changes.
 msg="rebuilding site `date`"
@@ -20,8 +26,11 @@ if [ $# -eq 1 ]
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
-git push origin master
 
 # Come Back up to the Project Root
 cd ..
+
+
+# Push source and build repos.
+git push origin master --recurse-submodules=check
+
